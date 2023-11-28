a5bfcbbc44ca76cab453deaa2fc8d572ea6ee7e2
abayomi <abayominice@gmail.com>
2023-11-28 12:44:36 +0100

    from my pc

--- public/scripts/construction.js
+++ public/scripts/construction.js
@@ -30,5 +30,20 @@ function myFunction() {
       const menu_item = document.querySelector(".menuitems3");
       menu_item.classList.remove("show3");
     }
+  }/* When the user clicks on the button for mobile screen (menu3),
+toggle between hiding and showing the dropdown content */
+
+function myFunction2() {
+  const menu_item = document.querySelector(".menuitems3");
+  const menu_toggler = document.querySelector(".icimg3");
+  menu_item.classList.toggle("show3");
+}
+
+function hideNav2() {
+  const main_menu_is_visible = document.querySelector(".show3");
+  if (main_menu_is_visible) {
+    const menu_item = document.querySelector(".menuitems3");
+    menu_item.classList.remove("show3");
   }
+}
   
--- public/scripts/procurement.js
+++ public/scripts/procurement.js
@@ -16,19 +16,19 @@ function myFunction() {
   }
   
   /* When the user clicks on the button for mobile screen (menu3),
-  toggle between hiding and showing the dropdown content */
-  
-  function myFunction() {
+toggle between hiding and showing the dropdown content */
+
+function myFunction2() {
+  const menu_item = document.querySelector(".menuitems3");
+  const menu_toggler = document.querySelector(".icimg3");
+  menu_item.classList.toggle("show3");
+}
+
+function hideNav2() {
+  const main_menu_is_visible = document.querySelector(".show3");
+  if (main_menu_is_visible) {
     const menu_item = document.querySelector(".menuitems3");
-    const menu_toggler = document.querySelector(".icimg3");
-    menu_item.classList.toggle("show3");
+    menu_item.classList.remove("show3");
   }
-  
-  function hideNav() {
-    const main_menu_is_visible = document.querySelector(".show3");
-    if (main_menu_is_visible) {
-      const menu_item = document.querySelector(".menuitems3");
-      menu_item.classList.remove("show3");
-    }
-  }
+}
   
